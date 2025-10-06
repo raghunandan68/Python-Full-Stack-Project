8813f79bf356361a7647591600b5d30cb4901fff
raghunandan68 <nallanraghu20@gmail.com>
2025-10-06 10:36:07 +0530

    Project committed

--- src/logic.py
+++ src/logic.py
@@ -2,7 +2,7 @@ import sys
 import os
 import requests
 import streamlit as st
-API_URL = "http://localhost:8000"
+API_URL = "http://127.0.0.1:8000"
 
 def register_via_api(username, email, password):
     payload = {"username": username, "email": email, "password": password}
